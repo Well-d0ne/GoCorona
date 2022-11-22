ff8d65817764774f0bbba860835366846513fc2d
Well-done <83390835+Well-d0ne@users.noreply.github.com>
2022-11-22 16:52:31 +0600

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-let burger = document.getElementById("burger")
-let menu = document.getElementById("menu")
-let back = document.querySelector('body');
-
-burger.addEventListener('click', () => {
-    burger.classList.toggle('active')
-    menu.classList.toggle('open')
-    back.classList.toggle('lock');
-})
